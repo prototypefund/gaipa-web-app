cdc7e75ff9f4c1f20d2bb86b2e12035b4c31eb45
MrTango2 <md@derico.de>
2018-07-20 19:03:18 +0300

    Add workbox route registration

---
+++ gaipa-client/src/sw.js
@@ -0,0 +1,15 @@
+
+if (workbox) {
+  workbox.routing.registerRoute(
+    new RegExp("(https?:\/\/gaipa\.org(\/app\/)()?)"),
+    workbox.strategies.staleWhileRevalidate()
+  );
+  workbox.routing.registerRoute(
+    new RegExp("(http?:\/\/localhost:.*(\/app\/)()?)"),
+    workbox.strategies.staleWhileRevalidate()
+  );
+
+  workbox.precaching.precacheAndRoute([]);
+} else {
+  console.log("Workbox didn't load! Offline support will not work.");
+}
