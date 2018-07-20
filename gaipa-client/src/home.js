3794becdcc32f1e2605bbcaff301a71f00ad5865
Juan <juanantonioguzmanhidalgo@gmail.com>
2018-07-20 16:13:28 +0200

    Home Static html

--- gaipa-client/src/home.js
+++ gaipa-client/src/home.js
@@ -3,43 +3,63 @@ export class Overview {
   farmingCycles = [
     {
       slug: 'planning',
-      title: 'Planning'
+      title: 'Planning',
+      image: 'images/planning.png',
+      notifications: '1'
     },
     {
       slug: 'preparing',
-      title: 'Preparing'
+      title: 'Preparing',
+      image: 'images/preparing.png',
+      notifications: ''
     },
     {
       slug: 'seeds',
-      title: 'Seeds'
+      title: 'Seeds',
+      image: 'images/seeds.png',
+      notifications: '4'
     },
     {
       slug: 'planting',
-      title: 'Planting'
+      title: 'Planting',
+      image: 'images/planting.png',
+      notifications: '4'
     },
     {
       slug: 'growing',
-      title: 'Growing'
+      title: 'Growing',
+      image: 'images/growing.png',
+      notifications: ''
     },
     {
       slug: 'harvesting',
-      title: 'Harvesting'
+      title: 'Harvesting',
+      image: 'images/harvesting.png',
+      notifications: '2'
     },
     {
       slug: 'transporting',
-      title: 'Transporting'
+      title: 'Transporting',
+      image: 'images/transporting.png',
+      notifications: '9'
     },
     {
       slug: 'processing',
-      title: 'Processing'
+      title: 'Processing',
+      image: 'images/mill.png',
+      notifications: '2'
     },
     {
       slug: 'storing',
-      title: 'Storing'
+      title: 'Storing',
+      image: 'images/storage.png',
+      notifications: '1'
     },
     {
       slug: 'selling',
-      title: 'Selling'
+      title: 'Selling',
+      image: 'images/market.png',
+      notifications: '2'
     }
   ];
 
