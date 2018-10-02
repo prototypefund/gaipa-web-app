352fb9a8e1901275067383d4a965e89c84a11f7e
MrTango <md@derico.de>
2018-10-02 12:07:33 +0200

    Finalize discourse-sso login

--- gaipa-client/src/discourse-sso.js
+++ gaipa-client/src/discourse-sso.js
@@ -10,8 +10,8 @@ import {
 
 @inject(AuthService)
 export class DiscourseSso extends BaseView {
-  constructor(authService) {
-    super();
+  constructor(authService, ...rest) {
+    super(...rest);
     this.auth = authService;
     this.message = 'Hello world';
     this.secret = 'f#jfUQ^yw9a*X@3%#Kn5xF#0k';  // TODO, put this in the backend
@@ -19,36 +19,34 @@ export class DiscourseSso extends BaseView {
   }
 
   activate(params) {
+    // /discourse-sso?sso=bm9uY2U9M2ZkNGRkNzk0NjM5ZWRlODcyNjU3MjMxNzU5MTQzYzImcmV0dXJuX3Nzb191cmw9aHR0cCUzQSUyRiUyRmNvbW11bml0eS5nYWlwYS5vcmclMkZzZXNzaW9uJTJGc3NvX2xvZ2lu&sig=f8897d30843d82ca4429528a2bdc11cbdfef85295b5650cfb97e08cc13f9b972
     let sso = params.sso;
     let sig = params.sig;
     let ssoHash = sha256.hmac(this.secret, sso);
-    console.log(sig);
-    console.log(ssoHash);
 
     if (ssoHash !== sig) {
+      console.log('HMAC signature invalid!');
       this.eventAggregator.publish(
         'status-message', {message: 'HMAC signature invalid!', type: 'error'}
       );
     } else {
+      this.user = this.auth.getUser();
       this.ssoDecoded = window.atob(sso);
-      console.log(this.ssoDecoded);
       let ssoParams = new URLSearchParams(this.ssoDecoded);
       let nonce = ssoParams.get('nonce');
       let returnSsoUrl = ssoParams.get('return_sso_url');
-      console.log(nonce);
-      console.log(returnSsoUrl);
-      this.user = this.auth.getUser();
-      ssoParams.set('name', this.user.fullname);
-      ssoParams.set('external_id', this.user.sub);
-      ssoParams.set('email', this.user.sub);
-      ssoParams.set('username', this.user.sub);
-      ssoParams.delete('return_sso_url');
-      this.ssoDecoded = ssoParams.toString();
+      let ssoNewParams = new URLSearchParams('');
+      ssoNewParams.append('name', this.user.fullname);
+      ssoNewParams.append('external_id', this.user.sub);
+      ssoNewParams.append('email', this.user.sub);
+      ssoNewParams.append('username', this.user.sub);
+      ssoNewParams.append('require_activation', false);
+      ssoNewParams.append('nonce', nonce);
+      this.ssoDecoded = ssoNewParams.toString();
       let ssoBase64 = window.btoa(this.ssoDecoded);
-      console.log(ssoBase64);
       let returnParamsString = encodeURI(ssoBase64);
-      console.log(returnParamsString);
-      let returnUrl = `${returnSsoUrl}?${returnParamsString}`;
+      let newSig = sha256.hmac(this.secret, returnParamsString);
+      let returnUrl = `${returnSsoUrl}?sso=${returnParamsString}&sig=${newSig}`;
 
       // redirect to community.gaipa.org SSO
       console.log('redirect to:' + returnUrl);
